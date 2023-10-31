a519ff334e90e74ca17bb76dea0e8b5de41afcb3
pandaren1013 <pandaren1013@outlook.com>
2023-10-31 14:56:07 +0800

    updated code

--- helpers/carouselImages.ts
+++ helpers/carouselImages.ts
@@ -2,14 +2,6 @@ import image1 from "../images/sites/1/1.jpg";
 import image2 from "../images/sites/1/2.jpg";
 import image3 from "../images/sites/1/3.jpg";
 import image4 from "../images/sites/1/4.jpg";
-import image5 from "../images/sites/1/1.png";
-import image6 from "../images/sites/1/2.png";
-import image7 from "../images/sites/1/3.png";
-
-import image8 from "../images/sites/2/1.png";
-import image9 from "../images/sites/2/2.png";
-import image10 from "../images/sites/2/3.png";
-
 interface image01Props {
   image: string;
   vercel_url: string;
@@ -41,5 +33,5 @@ const image05: image01Props = {
     vercel_url: "https://dustnboots.com/",
   };
 const images = [image01, image001,image02, image03, image04, image05];
-
+ 
 export default images;
