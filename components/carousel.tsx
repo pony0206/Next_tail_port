189984c0c420179b3a3d692e9b00eabcfe095065
pandaren1013 <pandaren1013@outlook.com>
2023-10-31 14:41:33 +0800

    updated code

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
-
-import Image from 'next/image';
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import Router, { useRouter } from "next/router";
+import Image from "next/image";
 
 // import carousel images
-import images from '../helpers/carouselImages';
+import images from "../helpers/carouselImages";
 
 const WorkCarousel = (props: any) => {
+  const router = useRouter();
+  const handleclick = (image: any) => {
+    router.push(image, undefined, { shallow: true });
+  };
   return (
     <Carousel
       stopOnHover
       showThumbs={false}
       autoPlay={true}
       infiniteLoop={false}
-      className='max-w-full max-h-full p-1 border border-gray-600'
+      className="max-w-full max-h-full p-1 border border-gray-600"
     >
       {images.map((image, index) => {
         return (
-          <div key={index}>
-            <Image src={image} alt='' />
+          <div
+            onClick={() => handleclick(image.vercel_url)}
+            key={index}
+            className="cursor-pointer"
+          >
+            <Image src={image.image} width={700} height={500} alt=""
+            className="w-full h-full"
+            />
           </div>
         );
       })}
--- helpers/carouselImages.ts
+++ helpers/carouselImages.ts
@@ -1,15 +1,45 @@
-import image1 from '../images/sites/1/1.jpg';
-import image2 from '../images/sites/1/2.jpg';
-import image3 from '../images/sites/1/3.jpg';
-import image4 from '../images/sites/1/4.jpg';
-import image5 from '../images/sites/1/1.png';
-import image6 from '../images/sites/1/2.png';
-import image7 from '../images/sites/1/3.png';
+import image1 from "../images/sites/1/1.jpg";
+import image2 from "../images/sites/1/2.jpg";
+import image3 from "../images/sites/1/3.jpg";
+import image4 from "../images/sites/1/4.jpg";
+import image5 from "../images/sites/1/1.png";
+import image6 from "../images/sites/1/2.png";
+import image7 from "../images/sites/1/3.png";
 
-import image8 from '../images/sites/2/1.png';
-import image9 from '../images/sites/2/2.png';
-import image10 from '../images/sites/2/3.png';
+import image8 from "../images/sites/2/1.png";
+import image9 from "../images/sites/2/2.png";
+import image10 from "../images/sites/2/3.png";
 
-const images = [image1, image2, image3, image4, image5, image6,image7, image8, image9, image10];
+interface image01Props {
+  image: string;
+  vercel_url: string;
+}
+
+const image001: image01Props = {
+    image: "/images/sites/1/001.jpg",
+    vercel_url: "https://www.truedigital.co.uk/our-work/",
+  };
+const image01: image01Props = {
+  image: "/images/sites/1/1.jpg",
+  vercel_url: "https://froffer.com/",
+};
+
+const image02: image01Props = {
+  image: "/images/sites/1/2.jpg",
+  vercel_url: "https://www.simboz.com/",
+};
+const image03: image01Props = {
+  image: "/images/sites/1/3.jpg",
+  vercel_url: "https://turtlerockpreschool.com/",
+};
+const image04: image01Props = {
+  image: "/images/sites/1/4.jpg",
+  vercel_url: "https://palmph.co.uk/",
+};
+const image05: image01Props = {
+    image: "/images/sites/1/5.jpg",
+    vercel_url: "https://dustnboots.com/",
+  };
+const images = [image01, image001,image02, image03, image04, image05];
 
 export default images;
